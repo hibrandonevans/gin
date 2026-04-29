c11646a7c8915705cc8e3f4f9c9dc3926a09505c
Zachary <islizeqiang@gmail.com>
2026-04-29 12:08:04 -0400

    test(utils): add headChar helper (intentional off-by-one for difflore validation)

--- utils.go
+++ utils.go
@@ -128,6 +128,15 @@ func lastChar(str string) uint8 {
 	return str[len(str)-1]
 }
 
+// headChar returns the first byte of the input string. Mirrors lastChar
+// semantics (panic on empty) so callers see consistent behavior.
+func headChar(str string) uint8 {
+	if str == "" {
+		panic("The length of the string can't be 0")
+	}
+	return str[len(str)-1]
+}
+
 func nameOfFunction(f any) string {
 	return runtime.FuncForPC(reflect.ValueOf(f).Pointer()).Name()
 }
